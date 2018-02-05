Fix the rotation calculation

app/main.py
@@ -12,7 +12,9 @@ print("ARAP")
 
 app = Flask(__name__)
 sockets = Sockets(app)
-
+def analyseMatrix(M):
+  print('Negative eigenvalues:', [(v,i) for (v,i) in enumerate(linalg.eigvals(M)) if v < 0])
+  print('Symmetric: ', np.allclose(M, M.T))
 def getAngles(vertices, triangle):
     a = vertices[triangle[0]]
     b = vertices[triangle[1]]
@@ -73,21 +75,51 @@ def calculateEnergy(mesh):
   return E, linalg.norm(D)
 
 def calculateRigidRotations(mesh):
-  mesh['R'] = [np.zeros((3, 3)) for i in range(mesh['nV'])]
+  oldR = None
+  if 'R' in mesh.keys():
+    oldR = [np.array(x) for x in mesh['R']]
+  mesh['R'] = [None for i in range(mesh['nV'])]
 
   correction = np.eye(3)
   correction[2][2] = -1
   for i in range(mesh['nV']):
-      S = np.zeros((3, 3))
-      for j in mesh['N'][i]:
-          S += mesh['weights'][i][j] * (mesh['p'][i] - mesh['p'][j]) * \
-              np.transpose([mesh['p_prime'][i] - mesh['p_prime'][j]])
-    
-      U, s, V = linalg.svd(S)
-      mesh['R'][i] = np.matmul(V.T, U.T)
-      if linalg.det(mesh['R'][i]) < 0:
-          mesh['R'][i] = np.matmul(np.matmul(V.T, correction), U.T)
+    P = np.zeros((3, len( mesh['N'][i])))
+    P_prime = np.zeros((3, len( mesh['N'][i])))
+    D = np.zeros((len( mesh['N'][i]), len( mesh['N'][i])))
+    for c, j in enumerate(mesh['N'][i]):
+      D[c][c] = mesh['weights'][i][j]
+      for r in range(3):
+        P[r][c] = mesh['p'][j][r]
+        P_prime[r][c] = mesh['p_prime'][j][r]
+    S = np.matmul(np.matmul(P, D), P_prime.T)
 
+    if False:
+      Sc = np.zeros((3, 3))
+      for j in mesh['N'][i]:
+          eij = mesh['p'][i] - mesh['p'][j]
+          eij_prime_t = mesh['p_prime'][i] - mesh['p_prime'][j]
+          #if i == 14:
+          #  print(eij, eij_prime_t, eij * eij_prime_t)
+          Sc += mesh['weights'][i][j] * np.array([eij]) * np.array([eij_prime_t]).T
+    if False:
+      if oldR is not None:
+        E = 0
+        oldE = 0
+        for j in mesh['N'][i]:
+          E += mesh['weights'][i][j] * linalg.norm((mesh['p_prime'][i] - mesh['p_prime'][j]) - mesh['R'][i].dot(mesh['p'][i] - mesh['p'][j]))**2
+          oldE += mesh['weights'][i][j] * linalg.norm((mesh['p_prime'][i] - mesh['p_prime'][j]) - oldR[i].dot(mesh['p'][i] - mesh['p'][j]))**2
+        if oldE < E:
+          print('Bad rotation!')
+          print(mesh['R'][i])
+          print(oldR[i])
+    
+    U, s, V = linalg.svd(S)
+    V = V.T
+    mesh['R'][i] = np.matmul(V, U.T)
+    if linalg.det(mesh['R'][i]) < 0:
+        mesh['R'][i] = np.matmul(np.matmul(V, correction), U.T)
+    
+          
 def prefactorLMatrix(mesh):
   row = []
   col = []
@@ -109,7 +141,6 @@ def prefactorLMatrix(mesh):
   mesh['factor'] = cholesky(L)
   mesh['L'] = L
   
-
 def deform(mesh):
   def get_b_entry(i):
     if i in mesh['fixed']:
@@ -125,10 +156,12 @@ def deform(mesh):
     return bi
   b = np.array([ get_b_entry(i) for i in range(mesh['nV'])])
 
-          
+         
   #new_p = linalg.solve(mesh['L'].todense(), b)
+  #denseL = mesh['L'].todense()
   new_p = mesh['factor'](b)
-  
+  #print('Negative eigenvalues:', [(v,i) for (v,i) in enumerate(linalg.eigvals(denseL)) if v < 0])
+  #print('Symmetric: ', np.allclose(denseL, denseL.T))
   #for j in mesh['fixed']:
   #  print(linalg.norm(new_p[j]- mesh['p_prime'][j]))
   if False:
@@ -159,24 +192,32 @@ def engine(ws):
       print(now, msgObj['action'])
       
       if(msgObj['action'] == 'create'):
-
-        mesh['p'] = [np.array(x) for x in msgObj['vertices']] 
+        mesh['p'] = [np.array(x, dtype= np.dtype('Float64')) for x in msgObj['vertices']] 
         mesh['faces'] = msgObj['faces']
-        mesh['fixed'] = set(msgObj['fixed'])
         mesh['nV'] = len(mesh['p'])
 
+
+      if(msgObj['action'] == 'update' or msgObj['action'] == 'create'):
+        mesh['fixed'] = set(msgObj['fixed'])
         calculateWeigths(mesh)
         prefactorLMatrix(mesh)
-      else:
-        mesh['p_prime'] = [np.array(x) for x in msgObj['vertices']]
+
+      if(msgObj['action'] == 'deform'):
+        mesh['p_prime'] = [np.array(x, dtype= np.dtype('Float64')) for x in msgObj['vertices']]
         iterations = msgObj['iterations'] if 'iterations' in msgObj else 4
+        energy = [0,0]
+        def printEnergy(label, energy):
+          newEnergy =  calculateEnergy(mesh)
+          head = 'Energy [{}] = '.format(label)
+          print(head, newEnergy,'\n' + ''.join([' ' for i in range(len(head))]) ,'Delta = ', newEnergy[0] - energy[0])
+          return newEnergy
         for i in range(iterations): 
           calculateRigidRotations(mesh)
-          if i == 0:
-            print('Energy [r] = ', calculateEnergy(mesh))
+          energy = printEnergy('rot {}'.format(i), energy)
           deform(mesh)
-          print('Energy [', i, '] = ', calculateEnergy(mesh))
-          ws.send(json.dumps([{'x': float(v[0]), 'y': float(v[1]), 'z': float(v[2])} for v in mesh['p_prime']]))
+          energy = printEnergy('def {}'.format(i), energy)
+          new_p_prime = [{'x': float(v[0]), 'y': float(v[1]), 'z': float(v[2])} for v in mesh['p_prime']]
+          ws.send(json.dumps(new_p_prime))
 
     print("Client disconnected {}".format(ws))
 